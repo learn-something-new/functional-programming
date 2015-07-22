5e89b4612082a7d046787bb31a884282541b86bd
Jeff Hann <jeff.hann@redspace.com>
2015-07-22 11:57:42 -0300

    more comments and examples

--- partial-eval/average-partial.hs
+++ partial-eval/average-partial.hs
@@ -1,8 +1,19 @@
+-- average accepts three integers and cacluates the average
+-- in: Integer, Integer, Integer
+-- out: Integer
 average :: Integer -> Integer -> Integer -> Integer
 average x y z = (x + y + z) `div` 3
 
+-- first passes a single integer to average and stores the 
+-- partial evaluation that is returned
 first = average 1
+
+-- second works the same way first does, passing a single integer
+-- and storing the partially evaluated result
 second = first 2
+
+-- third passes the final integer and stores the computed value of average
+-- which in this case is the average of all three integers being provided
 third = second 3
 
 main = print third
--- partial-eval/average.hs
+++ partial-eval/average.hs
@@ -1,3 +1,6 @@
+-- average accepts three integers and cacluates the average
+-- in: Integer, Integer, Integer
+-- out: Integer
 average :: Integer -> Integer -> Integer -> Integer
 average x y z = (x + y + z) `div` 3
 
